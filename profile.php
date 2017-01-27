e1e392af40fd4fbdbaef5257ce341887fe526072
Bjorn Keyser <bjornkeyser@gmail.com>
2017-01-27 20:34:12 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,19 +7,23 @@ $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if (!isset($_SESSION['id'])){
   header("Location: ../index.php");
 }
+$query_verifycode = "SELECT * FROM verify WHERE uid = ".$_SESSION['id'];
+$result_verifycode = mysqli_query($connection, $query_verifycode);
+$obj_verifycode = mysqli_fetch_object($result_verifycode);
 $query = "SELECT * FROM users WHERE id = '$uid'";
 $result = mysqli_query($connection, $query, MYSQLI_ASSOC);
 if (!$obj = mysqli_fetch_object($result))
 	{
 	header("Location :404.php");
 	}
+
 ?>
 	<!DOCTYPE html>
 	<html lang="en">
 
 	<head>
 		<meta charset="UTF-8">
-		<title>Chitchathomepage</title>
+		<title><?php echo $obj->first." ".$obj->last ?></title>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
 		<link rel="stylesheet" href="css/style.css">
@@ -27,36 +31,9 @@ if (!$obj = mysqli_fetch_object($result))
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<link href="bootstrap-tour.min.css" rel="stylesheet">
 		<script src="bootstrap.min.js"></script>
-		<script src="../tour.js"></script>
-		<script>
-			function updateCountdown() {
-				// 140 is the max message length
-				//var remaining = 200 - jQuery('#message').val().length;
-				//jQuery('#remaining').text(remaining + ' characters remaining.');
-			}
-			$(document).ready(function() {
-						$("#changeProfile").click(function() {
-							$("#changeProfile").replaceWith("<button class='btn btn-info' type='submit'>Save</button><a href='profile.php?uid=<?= $_SESSION['id'] ?>'>Cancel</a>");
-							$("#profilePic").replaceWith("<img src='<?=$obj->img?>' id='profilePic' class='img-circle' height='65' width='65' alt='Avatar'><br><form action='php/upload.php' method='POST' enctype='multipart/form-data'>Select image to upload:<input type='file' name='fileToUpload' id='fileToUpload'><input type='submit' value='Upload Image' name='submit'></form'");
-							$("#fullName").replaceWith("");
-							$("#email").replaceWith("<input placeholder='First name' value='<?= $obj->first ?>' name='first'></input><br><input placeholder='Last name' value='<?= $obj->last ?>' name='last'></input><br><input placeholder='Email' value='<?= $obj->email ?>' name='email'></input>");
-							$("#uname").replaceWith("<input placeholder='Username' value='<?= $obj->uname ?>' name='uname'></input>");
-							$("#bio").replaceWith("<input placeholder='Bio' value='<?= $obj->bio ?>' name='bio'></input>");
-
-
-						});
-						/*$("#followers").click(function(){
-				$("#followers").replaceWith("<?php echo $obj_followers_users->uname ?>");
-    });
-		$("#following").click(function(){
-				$("#following").replaceWith("<?php echo $obj_following_users->uname ?>");
-    });
-		updateCountdown();
-    $('#message').change(updateCountdown);
-    $('#message').keyup(updateCountdown);
-});
-		</script>
-	</head>
+		<link rel="shortcut icon" type="image/png" href="favicon.png">
+		
+			</head>
 
 	<body>
 		<script>
@@ -72,13 +49,27 @@ include "includes/header.php";
 
 ?>
 			<!--Container-->
-			<div class='container text-center' style='margin-top:5vh;'>
+			<div class='container text-center' style='margin-top:5vh;'>	
 				<h1>
-<?php if ($uid === $_SESSION['id'])
-	{
-	echo "Hi there, ";
+<?php
+	if ($uid === $_SESSION['id']){
+		echo "Hi there, ";
 	} ?>
 				<?=$obj->first?> <?=$obj->last?></h1>
+				<h3>
+<?php
+	if ($ver === $obj_verifycode->code && isset($ver)){
+		include 'php/connect.php';
+		$query_verify = "UPDATE users SET verified = 2 WHERE id = ".$_SESSION['id'];
+		$result_verify = mysqli_query($connection, $query_verify);
+		?> <span class='alert alert-success'>Your e-mail is now verified</span>
+<?php	} 	
+	include 'php/connect.php';
+	$obj_updated = mysqli_fetch_object(mysqli_query($connection, "SELECT * FROM users WHERE id = '$uid'"));
+	if ($obj_updated->verified === '1' && $obj->id===$_SESSION['id']){ ?>
+		<span class='alert alert-danger'>Your e-mail has not yet been verified</span><br>
+	<?php }	?>
+				</h3>
 				<p style='color:grey'>@<?=$obj->uname?></p>
 				<div class='row'>
 					<div class='col-md-3'>
@@ -99,18 +90,15 @@ include "includes/header.php";
 									echo "";		}
 									else { echo "<button>ontvolg</button>";}*/?>
 								<br>
-								<a href='' data-toggle='modal' data-target='#imgModal'><img src='uploads/<?=$obj->img?>' id='profilePic' height='100' width='100' alt='Avatar'></a>
+								<a href='' data-toggle='modal' data-target='#imgModal' id='modal'><img src='uploads/<?=$obj->img?>' id='profilePic' height='100' width='100' alt='Avatar'></a>
 								<div id='imgModal' class='modal fade' role='dialog'>
 									<span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
 									<img class="modal-content" id="img01" src='uploads/<?=$obj->img?>'>
-									<div id="caption"></div>
 								</div>
 								<p id='email'>
 									<?=$obj->email?>
 								</p>
-								<p id='uname'>@
-									<?=$obj->uname?>
-								</p>
+								<p id='uname'>@<?=$obj->uname?></p>
 								<p id='bio'>Bio:
 									<?=$obj->bio?>
 								</p>
@@ -133,9 +121,7 @@ include "includes/header.php";
 							</ul>
 						</div>
 					</div>
-					<div class='col-md-6'>
-						<!--<div class='row'>
-							<div class='col-md-12'>-->
+					<div class='col-md-6' style='background-color:white'>
 						<div class='panel panel-default text-left'>
 							<div class='panel-body'>
 								<div class='form-group'>
@@ -154,14 +140,12 @@ if ($posted == 1){?>
 										<button class='btn btn-info' type='submit' name='cheetsubmit'>Post chit</button>
 										<span id='remaining'>200 characters remaining</span>
 									</form>
-									<!--</div>
-									</div>-->
 								</div>
 							</div>
 						</div>
 						<?=getCheets($connection, $obj_users, $result_cheets, $actual_link)?>
 					</div>
-				</div>
+				<!--</div>-->
 				<div class='col-md-3'>
 					<div class='list-group'>
 						<h4>Popular users</h4>
@@ -173,6 +157,20 @@ if ($posted == 1){?>
 include "includes/footer.php";
 
 ?>
+		
+		<script>
+			$(function() {
+						$("#changeProfile").on("click", function() {
+							$(this).replaceWith("<button class='btn btn-info' type='submit'>Save</button><a href='profile.php?uid=<?= $_SESSION['id'] ?>'>Cancel</a>");
+							$("#modal").contents().unwrap();
+							$("#profilePic").replaceWith("<img src='uploads/<?=$obj->img?>' id='profilePic' class='img-circle' height='65' width='65' alt='Avatar'><br><form action='php/upload.php' method='POST' enctype='multipart/form-data'>Select image to upload:<input type='file' name='fileToUpload' id='fileToUpload'><input class='btn btn-info' type='submit' value='Upload Image' name='submit'></form'");
+							$("#fullName").replaceWith("");
+							$("#email").replaceWith("<input placeholder='First name' value='<?= $obj->first ?>' name='first'></input><br><input placeholder='Last name' value='<?= $obj->last ?>' name='last'></input><br><input placeholder='Email' value='<?= $obj->email ?>' name='email'></input>");
+							$("#uname").replaceWith("<input placeholder='Username' value='<?= $obj->uname ?>' name='uname'></input>");
+							$("#bio").replaceWith("<input placeholder='Bio' value='<?= $obj->bio ?>' name='bio'></input>");
+						});
+			});
+		</script>
 	</body>
 
 	</html>
