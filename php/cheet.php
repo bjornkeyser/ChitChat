9a221d43b11e4b68631edf9b59c98482a8368173
Bjorn Keyser <bjornkeyser@gmail.com>
2017-01-31 12:01:48 +0100

    Update cheet.php

--- php/cheet.php
+++ php/cheet.php
@@ -55,7 +55,7 @@ function postCheet($connection, $obj_users){
     header("Location: ../profile.php?uid=".$_SESSION["id"]."&posted=1");
   }
 }
-$query_cheets = "select * from cheets left join tags on cheets.cid = tags.cid join users on users.id = cheets.uid where uid= ".$obj_users->id." or tags.tag = '".$obj_users->uname."' order by cheets.date desc";
+$query_cheets = "select *, cheets.cid as cheetid from cheets left join tags on cheets.cid = tags.cid join users on users.id = cheets.uid where uid= ".$obj_users->id." or tags.tag = '".$obj_users->uname."' order by cheets.date desc";
 $result_cheets = mysqli_query($connection, $query_cheets);
 
 function getCheets($connection, $obj_users, $result_cheets, $actual_link){
@@ -63,15 +63,16 @@ function getCheets($connection, $obj_users, $result_cheets, $actual_link){
   parse_str($_SERVER['QUERY_STRING']);
   while ($obj_cheets = mysqli_fetch_object($result_cheets)){
 		$string = stripslashes(str_replace("\r\n","<br />",(nl2br($obj_cheets->cheet))));
-		$arr = explode(" ", $string);
-    echo "<div class='media text-left'>
-    <div class='media-left'>
-      <img src='"."../uploads/".$obj_cheets->img."' class='media-object' style='width:45px'>
-    </div>
-    <div class='media-body'>
-      <h6 class='media-heading' style='font-size:110%;'><a href='http://chitchatonline.esy.es/profile.php?uid=$obj_cheets->id'>@".$obj_cheets->uname."</a><small><i> $obj_cheets->date</i></small></h4>
-      <p style='font-size:120%'>";
-		for ($i=0;$i<count($arr);$i++){
+		$arr = explode(" ", $string); ?>
+<div class='media text-left'>
+	<div class='media-left'>
+		<img src='../uploads/<?=$obj_cheets->img?>' class='media-object' style='width:45px'>
+	</div>
+	<div class='media-body'>
+		<h6 class='media-heading' style='font-size:110%;'><a href='http://chitchatonline.esy.es/profile.php?uid=<?=$obj_cheets->id?>'>@<?=$obj_cheets->uname?></a><small><i> <?=$obj_cheets->date?></i></small></h4>
+      <p style='font-size:120%'>
+<?php //hashtag and tag linking
+			for ($i=0;$i<count($arr);$i++){
 			if (substr($arr[$i],0,1)==="#"){
 					$arr[$i] = "<a href='http://chitchatonline.esy.es/php/search.php?htag=".substr($arr[$i], 1)."&uid=".$_SESSION['id']."'>".$arr[$i]."</a>"; 
 				}
@@ -80,32 +81,64 @@ function getCheets($connection, $obj_users, $result_cheets, $actual_link){
 				}
 		}
 $string = implode(" ",$arr);
-echo $string."</p >";
-			echo "cid=".$obj_cheets->cid.$likes."
+echo $string ?>
+	</p >
       <form style='display: inline' action='php/like.php' method='POST'>
-				".$obj_cheets->likes."
-				<input type='hidden' value='".$obj_cheets->cid."' name ='cid'>
-				<input type='hidden' value='".$actual_link."' name ='link'>
-				<button name='likebtn' type='submit' class='btn' style='border:none'><i class='fa fa-thumbs-o-up'></i></button>
+				<?=$obj_cheets->likes?>
+				<input type='hidden' value='<?=$obj_cheets->cheetid?>' name ='cid'>
+				<input type='hidden' value='<?=$actual_link?>' name ='link'>
+				<button name='likebtn' type='submit' class='btn' style='border:none' <?php //if (1===1) {echo "disabled";} ?>><i class='fa fa-thumbs-o-up'></i></button>
 			</form>
       <form style='display: inline' action='php/dislike.php' method='POST'>
-				".$obj_cheets->dislikes."
-				<input type='hidden' value='".$obj_cheets->cid."' name ='cid'>
-				<input type='hidden' value='".$actual_link."' name ='link'>
+				<?=$obj_cheets->dislikes?>
+				<input type='hidden' value='<?=$obj_cheets->cheetid?>' name ='cid'>
+				<input type='hidden' value='<?=$actual_link?>' name ='link'>
 				<button name='dislikebtn' type='submit' class='btn' style='border:none'><i class='fa fa-thumbs-o-down'></i></button>
 			</form>
-      <a href='#' id='comment'>Comment</a>
-     </div>";
-      if ($_SESSION['id']===$obj_cheets->uid){
-      echo "<div class='media-right'>
+      <a id='comment' onclick="commentBox('<?= $obj_cheets->cheetid ?>')">Comment</a>
+		<!-- Reply text box -->
+			<div class='panel panel-default text-left' style='display:none' id='commentbox<?= $obj_cheets->cheetid ?>'>
+				<div class='panel-body'>
+					<div class='form-group'>
+						<form action='php/postreply.php' method='POST'>
+							<textarea maxlength='200' class='form-control' rows='3' name='reply' required></textarea>
+							<input type="hidden" name='cid' value='<?= $obj_cheets->cheetid ?>'>
+							<input type="hidden" name='link' value='<?= $actual_link ?>'>
+							<button class='btn btn-info' type='submit' id='replybtn'>Reply</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		<!-- Nested media objects -->
+		<?php
+			$i=0;
+			$query_reply = "SELECT * FROM replies JOIN users ON replies.uid = users.id WHERE comment_on = ".$obj_cheets->cheetid;
+			$result_reply = mysqli_query($connection, $query_reply);
+			while ($obj_reply = mysqli_fetch_object($result_reply)){
+			$i++;
+				if (mysqli_num_rows(mysqli_query($connection, $query_reply))!==0){
+		?>
+					<div class="media">
+            <div class="media-left">
+              <img src="../uploads/<?= $obj_reply->img ?>" class="media-object" style="width:45px">
+            </div>
+            <div class="media-body">
+							<h6 class='media-heading' style='font-size:110%;'><a href='http://chitchatonline.esy.es/profile.php?uid=<?=$obj_reply->id?>'>@<?=$obj_reply->uname?></a><small><i> <?=$obj_reply->date?></i></small></h4>
+								<p style='font-size:120%'><?= $obj_reply->reply ?></p>
+            </div>
+          </div>
+		<?php } ?>
+	<?php } ?>
+     </div>
+      <?php if ($_SESSION['id']===$obj_cheets->uid){ ?>
+      <div class='media-right'>
       <a href='' data-toggle='modal' data-target='#editModal'>Edit</a>
       <form action ='php/deletechit.php' method='POST'>
-        <input type='hidden' value='".$obj_cheets->cid."' name ='cid'></input>
+        <input type='hidden' value='<?=$obj_cheets->cheetid?>' name ='cid'></input>
         <button class='btn btn-info' name='deletechit' type='submit'>Delete</button>
       </form>
        <div id='editModal' class='modal fade' role='dialog'>
        <div class='modal-dialog'>
-
           <div class='modal-content'>
             <div class='modal-header'>
               <button type='button' class='close' data-dismiss='modal'>&times;</button>
@@ -114,21 +147,20 @@ echo $string."</p >";
             <div class='modal-body'>
               <form action='php/editchit.php' method='POST'>
               <input type='text' name='newContent'></input>
-              <input type='hidden' value='".$obj_cheets->cid."' name='cid'></input>
+              <input type='hidden' value='<?=$obj_cheets->cheetid?>' name='cid'></input>
               <button type='submit' class='btn btn-info'>Edit</button>
               </form>
             </div>
             <div class='modal-footer'>
               <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-      </div>";
-      }
-		}
-      echo "</div></div>";
-  }
+      			</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<?php } //sluit de edit en delete knop af ?> </div>
+<?php	} /* sluit de while loop af */ ?>
+<?php } /*sluit de functie af */
 function popularUsers($connection, $actual_link){
   include 'connect.php';
   $query_popular = "SELECT COUNT( following_id ) AS followers, following_id, users.* FROM follow JOIN users ON following_id = users.id GROUP BY following_id ORDER BY followers DESC LIMIT 5";
@@ -136,42 +168,42 @@ function popularUsers($connection, $actual_link){
   while ($obj_popular = mysqli_fetch_object($result_popular)){ ?>
   									<div class="list-group-item">
   									<div class="media-left media-middle">
-											<a href="#">
+											<a href="http://chitchatonline.esy.es/profile.php?uid=<?php echo $obj_popular->id ?>">
 												<img class="media-object" src="uploads/<?php echo $obj_popular->img; ?>" style='width:65px'>
     									</a>
   									</div>
 										<div class="media-body">
 											<h6 class="media-heading"><a href='http://chitchatonline.esy.es/profile.php?uid=<?php echo $obj_popular->id ?>'><?php echo $obj_popular->uname; ?></a></h6>
-<?php
+			<?php
   if ($_SESSION['id']!==$obj_popular->id && checkFollowing($obj_popular->id, $connection)!==true){ ?>
-                  <form id='followform' action='php/follow.php' method='POST'>
-                    <input type='hidden' value='<?php echo $obj_popular->id; ?>' name ='id'>
-                    <input type='hidden' value='<?= $actual_link ?>' name ='link'>
-                    <button class='btn btn-info' name='follow' type='submit' id='followbtn'>Follow</button>
-                  </form>
-                </div>
-								</div>
-              <?php  } else if ($_SESSION['id']===$obj_popular->id){echo "</div></div>";} else { ?>
-                          <form action='php/unfollow.php' method='POST'>
-                            <input type='hidden' value='<?php echo $obj_popular->id; ?>' name='id'>
-														<input type='hidden' value='<?= $actual_link ?>' name ='link'>
-                            <button class='btn btn-default' type='submit'>Unfollow</button>
-                            </form>
-                            </div>
-														</div> 
- <?php } } 
+				<form id='followform' action='php/follow.php' method='POST'>
+					<input type='hidden' value='<?php echo $obj_popular->id; ?>' name='id'>
+					<input type='hidden' value='<?= $actual_link ?>' name='link'>
+					<button class='btn btn-info' name='follow' type='submit' id='followbtn'>Follow</button>
+				</form>
+		</div>
+	</div>
+	<?php  } else if ($_SESSION['id']===$obj_popular->id){echo "</div></div>";} else { ?>
+	<form action='php/unfollow.php' method='POST'>
+		<input type='hidden' value='<?php echo $obj_popular->id; ?>' name='id'>
+		<input type='hidden' value='<?= $actual_link ?>' name='link'>
+		<button class='btn btn-default' type='submit'>Unfollow</button>
+	</form>
+	</div>
+	</div>
+	<?php } } 
 } 
 function trendingTopic($connection){
 	include 'connect.php';
 	echo"Last few days";
-	$query_trending = "SELECT COUNT(hashtag) AS count, hashtag FROM hashtags JOIN cheets ON cheets.cid = hashtags.cid WHERE date >= curdate() - INTERVAL DAYOFWEEK(curdate())+1 DAY
-AND date <= curdate() GROUP BY hashtag ORDER BY count DESC LIMIT 5";
+	//$query_trending = "SELECT COUNT(hashtag) AS count, hashtag FROM hashtags JOIN cheets ON cheets.cid = hashtags.cid WHERE date >= curdate() - INTERVAL DAYOFWEEK(curdate())+1 DAY
+//AND date <= curdate() GROUP BY hashtag ORDER BY count DESC LIMIT 5";
+	$query_trending = "SELECT COUNT(hashtag) AS count, hashtag FROM hashtags JOIN cheets ON cheets.cid = hashtags.cid GROUP BY hashtag ORDER BY count DESC LIMIT 5";
 	$result_trending = mysqli_query($connection, $query_trending);
 	while ($obj_trending = mysqli_fetch_object($result_trending)){ ?>
-		<a href='http://chitchatonline.esy.es/php/search.php?htag=<?php echo $obj_trending->hashtag; ?>&uid=<?php echo $_SESSION['id'] ?>&tt=1'>
-			<li class='list-group-item'>#<?php echo mb_strimwidth("$obj_trending->hashtag", 0, 18, "..."); ?><span class='badge'><?php echo $obj_trending->count; ?></span>
-			</li>
-		</a>
+	<a href='http://chitchatonline.esy.es/php/search.php?htag=<?php echo $obj_trending->hashtag; ?>&uid=<?php echo $_SESSION['id'] ?>&tt=1'>
+		<li class='list-group-item'>#<?php echo mb_strimwidth("$obj_trending->hashtag", 0, 18, "..."); ?><span class='badge'><?php echo $obj_trending->count; ?></span></li>
+	</a>
 	<?php }
 }
 function checkFollowing($followingid, $connection){
@@ -198,3 +230,17 @@ function dislikeChit($connection, $cid){
   }
 }
 ?>
+<script>
+	function commentBox(a){
+		$("#commentbox"+a).toggleClass("show");
+	}
+	  /*$("#replybtn").click(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'POST',
+        url: 'postreply.php',
+        data: $('form').serialize(),
+      });
+      $("#msg").val("");
+    });*/
+</script>
